b5e89bc723113af90794cbc76ed18ea72488fb2d
Brian Wyatt <brianlwyatt@gmail.com>
2023-04-20 22:10:56 -0300

    new project added

--- src/components/ProjectComponents/WorkCardData.js
+++ src/components/ProjectComponents/WorkCardData.js
@@ -1,28 +1,28 @@
-import pro1 from '../../assets/snowbird.png';
-import pro2 from '../../assets/cooking.png';
-import pro3 from '../../assets/scheduler.jpeg';
+import snowbird from '../../assets/snowbird.png';
+import whatsCooking from '../../assets/cooking.png';
+import bodyBuddy from '../../assets/body-buddy.png';
 
 const ProjectCardData = [
   {
-    imgsrc: pro1,
+    imgsrc: snowbird,
     title: "The Snowbird Exchange",
     text: "A travel website that offers the five day weather forecast for your travel location, the current exchange rate to Canadian dollars, and dynamic images from your destination. Trial version, search London, Paris, Miami, Cancun and Rio de Janeiro to explore functionality.",
     view: "https://meduion.github.io/the-snowbird-exchange/",
     source: "https://github.com/Meduion/the-snowbird-exchange",
   },
   {
-    imgsrc: pro2,
-    title: "What's Cooking",
-    text: "A recipe finding and storage website tied to a third party API. Users can create an account, login, and save their recipes to their account. The interface offers functionality to create and save your own recipes, as well as linking to external sites for the full recipes.",
+    imgsrc: bodyBuddy,
+    title: "Body Buddy",
+    text: "Fitness app built in React with Next.js 13.2, Prisma, and MongoDB. Upon creating an account, users can construct their own workouts by calling a third party API to view different exercises. They then build a workout from these exercises and save it to the application's database.",
     view: "https://whats--cooking.herokuapp.com/",
     source: "https://github.com/Meduion/whats-cooking"
   },
   {
-    imgsrc: pro3,
-    title: "Day by Day",
-    text: "A basic workday scheduling app that allows you to add tasks for a standard workday and save them in local storage, available when returning to the page later. The calendar and time update automatically to the current time and the time slots will change color as they pass.",
-    view: "https://meduion.github.io/day-by-day/",
-    source: "https://github.com/Meduion/day-by-day"
+    imgsrc: whatsCooking,
+    title: "What's Cooking",
+    text: "A recipe finding and storage website tied to a third party API. Users can create an account, login, and save their recipes to their account. The dynamic interface offers functionality and to create and save your own recipes, as well as linking to external sites for the full recipes.",
+    view: "https://whats--cooking.herokuapp.com/",
+    source: "https://github.com/Meduion/whats-cooking"
   },
 ];
 
